fac49625edf6213903757375a59418b557b30c2b
Coder-Mehedi <mehedi5051@gmail.com>
2021-07-06 11:32:42 +0600

    fix done

--- src/navigator/StackNavigator.js
+++ src/navigator/StackNavigator.js
@@ -1,22 +1,17 @@
-import React from 'react'
+import React from 'react';
 import Welcome from '../screen/WelcomeScreen';
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './TabNavigator';
-
+import Characters from '../screen/CharacterScreen';
 
 const Stack = createStackNavigator();
 const MainStackNavigator = () => {
-    return (
-        <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome}/>
-        <Stack.Screen name="Base" component={TabNavigator}/>
-  
-        </Stack.Navigator>
-       
-  
-        
-     
-    );
-  };
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Welcome' component={Characters} />
+      <Stack.Screen name='Base' component={TabNavigator} />
+    </Stack.Navigator>
+  );
+};
 
-export default MainStackNavigator
+export default MainStackNavigator;
--- src/screen/CharacterScreen.js
+++ src/screen/CharacterScreen.js
@@ -1,48 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, Text, View } from 'react-native'
-import { gql, useQuery } from "@apollo/client";
-import client from './../api/apollo-client'
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { gql, useQuery } from '@apollo/client';
+import client from './../api/apollo-client';
 
 const getCharacter = gql`
-{
-  
-    characters{
-    
-        results{
-          id,
-          name,
-          gender,
-          status,
-          image,
-        }
+  {
+    characters {
+      results {
+        id
+        name
+        gender
+        status
+        image
+      }
     }
-}
+  }
 `;
 
-
 function Character({ Component, pageProps }) {
-    const { loading, error, data } = useQuery(getCharacter,{variables:{},});
-    if (loading) return <p>Loading ...</p>;
-   if (error) 
-    return <p>Error: {error.message}</p>;
-    console.log(data);
+  const { loading, error, data } = useQuery(getCharacter, { variables: {} });
+  if (data) console.log('data is', data);
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
 
-    
+  return (
+    <div>
+      {data.characters.results.map((character) => (
+        <div key={character.id}>
+          <h3>{character.id}</h3>
+          <p>
+            {character.name} - {character.gender} - {character.image}
+          </p>
+          <h3>{character.status}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-    return(
-        <div >
-        {data.characters.map((character) => (
-          <div key={character.id} >
-            <h3>{character.id}</h3>
-            <p>
-              {character.name} - {character.gender} - {character.image}
-            </p>
-            <h3>{character.status}</h3>
-          </div>
-        ))}
-      </div>
-      )
-      
-        }    
-
-export default Character
+export default Character;
